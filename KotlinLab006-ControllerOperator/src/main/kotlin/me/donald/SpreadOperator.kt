ee626d823d9f951ff20e87cabc4b4a1337e1001e
donald-dh <rlaeogh222@gmail.com>
2019-09-04 10:03:12 +0900

    가변인자 타입 변환 관련 추가

--- KotlinLab006-ControllerOperator/src/main/kotlin/me/donald/SpreadOperator.kt
+++ KotlinLab006-ControllerOperator/src/main/kotlin/me/donald/SpreadOperator.kt
@@ -6,20 +6,35 @@ package me.donald
  * - 오직 배열만 가능1
  */
 fun main(args: Array<String>) {
-    val array = arrayOf(10, 20, 30)
-    someSpread(*array)
+    val array = arrayOf(10, 20, 30)     // Integer로 변환
+    someSpread1(*array)
+
+    someSpreadInt(*array.toIntArray())
+    someSpreadInteger(*array)
+
+    val intArrayOf = intArrayOf(10, 20, 30) // int로 변환
+    someSpread1(intArrayOf)
 
     val list = listOf(1, 2, *array, 100, 200)
-    someSpread(*list.toTypedArray())
+    someSpread1(*list.toTypedArray())
 
     val array2 = arrayOf("hello", "world")
-    someSpread(*array2)
+    someSpread2(*array2)
 }
 
-fun<Int> someSpread(vararg arg: Int) {
+fun someSpreadInt(vararg arg: Int) {        // int로 변환
     arg.forEach { a -> println(a) }
 }
 
-fun someSpread(vararg arg: String) {        // 가변 인수 기본 값은 String?
+fun someSpreadInteger(vararg arg: Int?) {   // Integer로 변환
     arg.forEach { a -> println(a) }
 }
+
+fun <T> someSpread1(vararg arg: T) {        // 제네릭 타입
+    arg.forEach { a -> println(a) }
+}
+
+fun someSpread2(vararg arg: String) {        // 고정 타입
+    arg.forEach { a -> println(a) }
+}
+
