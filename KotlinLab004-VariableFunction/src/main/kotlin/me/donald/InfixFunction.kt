2c4231db377eb3945a4a2ba46fe2206e21a6e7ad
donald-dh <rlaeogh222@gmail.com>
2019-09-04 09:56:09 +0900

    Data 객체에 관한 중위함수 추가

--- KotlinLab004-VariableFunction/src/main/kotlin/me/donald/InfixFunction.kt
+++ KotlinLab004-VariableFunction/src/main/kotlin/me/donald/InfixFunction.kt
@@ -8,6 +8,9 @@ package me.donald
 // 중위 표현식에 사용되는 함수 선언 가능
 infix fun Int.myFun(x: Int): Int = x * x
 
+data class Test(val i : Int)
+infix fun Test.myFun(x: Int): Int = x * x
+
 // infix 함수는 클래스의 확장 함수이거나 클래스의 멤버 함수로 선언 가능
 infix fun FunClass.myFun(x: Int): FunClass = FunClass()
 
@@ -24,4 +27,6 @@ fun main() {
 
     println(10 myFun 10)
     println(10.myFun(10))
+
+    println(Test(1).myFun(10))
 }
